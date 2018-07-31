3534f65c2a16387cdb42cd3ee3f3663bdca99619
Atef Ben Ali <atef.bettaib@gmail.com>
2018-07-31 16:03:18 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var Crawler = require("crawler");
+
+var websites = process.argv.slice(2);
+
+console.log(websites);
+
+
+var c = new Crawler({
+    maxConnections: 10,
+    // This will be called for each crawled page
+    callback: function(error, res, done) {
+        if (error) {
+            console.log(error);
+        } else {
+            var $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            console.log($("title").text());
+        }
+        done();
+    }
+});
+
+websites.forEach(
+	function(website) {
+		c.queue(website);
+	}
+);
+
+// Queue just one URL, with default callback
+c.queue('http://www.amazon.com');
+// Queue a list of URLs
+c.queue(['http://www.google.com/', 'http://www.yahoo.com']);
+// Queue URLs with custom callbacks & parameters
+// c.queue([{
+//     uri: 'http://parishackers.org/',
+//     jQuery: false,
+//     // The global callback won't be called
+//     callback: function(error, res, done) {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log('Grabbed', res.body.length, 'bytes');
+//         }
+//         done();
+//     }
+// }]);
+// Queue some HTML code directly without grabbing (mostly for tests)
+c.queue([{
+    html: '<p>This is a <strong>test</strong></p>'
+}]);
