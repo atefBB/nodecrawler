9552f11b57d7192831721293646bcbf78273d52c
Atef Ben Ali <atef.bettaib@gmail.com>
2018-08-01 06:39:43 +0100

    feat: add more `user agents`

--- index.js
+++ index.js
@@ -7,7 +7,29 @@ console.log(websites);
 
 var c = new Crawler({
     maxConnections: 10,
-    userAgent: ['SeoRank Bot 0.1', 'Novatis Bot 0.1'],
+    userAgent: [
+    	"SeoRank Crawler: SeoRankBot/0.1 (+https://www.seorank.tn/contact-referencement-web-tunisie/)", 
+    	"Novatis Crawler: NovatisBot/0.1 (+https://www.novatis.tn/contact-agence-novatis/)",
+    	"APIs-Google (+https://developers.google.com/webmasters/APIs-Google.html)",
+        "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+        "Googlebot/2.1 (+http://www.googlebot.com/bot.html)",
+        "Mozilla/5.0 (compatible; Googlebot/2.1; startmebot/1.0; +https://start.me/bot)",
+        "Googlebot/2.1 (+http://www.google.com/bot.html)",
+        "Mozilla/5.0 (compatible; Googlebot/2.1; http://www.google.com/bot.html)",
+        "Mozilla/5.0 (compatible;acapbot/0.1;treat like Googlebot)",
+        "Mozilla/5.0 (compatible; Googlebot/2.1; +http://import.io)",
+        "Googlebot-Image/1.0",
+        "Googlebot",
+        "Google Crawler: Googlebot/2.1 (+http://www.google.com/bot.html)",
+        "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)/Nutch-2.3.1",
+        "Googlebot/2.1; +http://www.google.com/bot.html)",
+        "OnPageBot (compatible; Googlebot 2.1; +https://bot.onpage.org/)",
+        "Mozilla/5.0 (compatible; Googlebot/2.1 +http://www.googlebot.com/bot.html)",
+        "Mozilla/5.0 (compatible;acapbot/0.1.;treat like Googlebot)",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko; Google Page Speed Insights) Chrome/27.0.1453 Safari/537.36 GoogleBot/2.1",
+        "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; Google Web Preview Analytics) Chrome/27.0.1453 Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.4 (KHTML, like Gecko; Google Page Speed Insights) Chrome/22.0.1229 Safari/537.4 GoogleBot/2.1"
+    ],
     rotateUA: true,
     // This will be called for each crawled page
     callback: function(error, res, done) {
