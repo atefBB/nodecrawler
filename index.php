5d449bfb20131a4f74870831e9419b532e54c30a
Atef Ben Ali <atef.bettaib@gmail.com>
2018-08-01 07:18:48 +0100

    feat: execute `nodejs` from php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-$command = "node index.js";
+
+$keywords = join(' ', array_slice($argv, 1));
+
+$command = "node index.js {$keywords}";
+
 exec($command, $output, $return);
 
 var_dump($output, $return);die;
